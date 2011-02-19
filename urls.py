24bb7ab856b3a99be88df147b45021a866238fea
Carl Whittaker <carl359@gmail.com>
2011-02-19 16:44:41 +0000

    Configured serving of static files.

--- urls.py
+++ urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import *
-
+from django.conf import settings
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     url(r'^$', 'wager.views.index', name='home'),
     url(r'^(?P<slug>[a-z0-9\-_]+)/$', 'wager.views.wager', name='wager'),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT})
 )
