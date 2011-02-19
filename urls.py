80e55657e927ff388f4e0c3696727743f68db9ad
David Moss <david.moss@hogarthww.com>
2011-02-19 16:25:46 +0000

    Trying to add admin

--- urls.py
+++ urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
+admin.autodiscover()
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
+    (r'^admin/', include(admin.site.urls)),
     url(r'^$', 'wager.views.index', name='home'),
 )
