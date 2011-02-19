820be4d2ad11722ffd3b725568cbe6431f6f1955
Kevin Morgan <kevin.morgan@hogarthww.com>
2011-02-19 11:04:22 +0000

    Adding proper django app engine stuff.

--- django/tests/regressiontests/staticfiles_tests/urls/default.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    url(r'^static/(?P<path>.*)$', 'django.contrib.staticfiles.views.serve'),
-)
