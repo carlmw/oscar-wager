21edbb5a94b1e91bb50a5bfb53ff3a9ce077c637
David Moss <david.moss@hogarthww.com>
2011-02-19 16:38:51 +0000

    Removing Admin

--- urls.py
+++ urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls.defaults import *
-from django.contrib import admin
-admin.autodiscover()
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
-    (r'^admin/', include(admin.site.urls)),
     url(r'^$', 'wager.views.index', name='home'),
     url(r'^(?P<slug>[a-z0-9\-_]+)/$', 'wager.views.wager', name='wager'),
 )
