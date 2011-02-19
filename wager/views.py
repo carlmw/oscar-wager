367b345f823528407db2515a571220fd1fd283df
Kevin Morgan <kevin.morgan@hogarthww.com>
2011-02-19 15:19:39 +0000

    Saving wager if form is valid.

--- wager/views.py
+++ wager/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 
 from wager.forms import WagerForm
+from wager.models import Wager
 
 def index(request):
     if request.method == 'POST':
         wager_form = WagerForm(data=request.POST)
         if wager_form.is_valid():
-            pass
+            wager_form.save()
     else:
         wager_form = WagerForm()
     return render_to_response('index.html', {'wager_form': wager_form}, context_instance=RequestContext(request))
