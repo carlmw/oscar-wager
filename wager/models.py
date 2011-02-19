4138af1bc007f83f8694235d8a097ca0f58133a3
Kevin Morgan <kevin.morgan@hogarthww.com>
2011-02-19 15:19:27 +0000

    Adding and removing fields.

--- wager/models.py
+++ wager/models.py
@@ -1,17 +1,14 @@
 from django.db import models
-from autoslug import AutoSlugField
 
 class Wager(models.Model):
     """Wager model detailing the agreement of participants"""        
     name = models.CharField(max_length=50, unique=True)
-    slug = AutoSlugField(populate_from=lambda instance: instance.name,
-                         unique=True,
-                         slugify=lambda value: value.replace(' ','-'))
+    slug = models.CharField(max_length=50, unique=True)
     
 class User(models.Model):
     """User model detailing the name and email address of the users in a wager"""
     name = models.CharField(max_length=50)
-    # wager = models.ForeignKey(Wager)
+    wager = models.ForeignKey(Wager)
 
 class Award(models.Model):
     """Award model detailing the name of the award"""    
@@ -21,11 +18,11 @@ class Entry(models.Model):
     """Entry model detailing the name of the entry(film, actor) 
        and a reference of where/why nominated(film, director)"""
     name = models.CharField(max_length=50)
-    # award = models.ForeignKey(Award)
+    award = models.ForeignKey(Award)
     reference = models.CharField(max_length=50, null=True)
     
 class Vote(models.Model):
     """Vote model detailing the selection of votes made against a user for a """
-    # entry = models.ForeignKey(Entry)
-    # wager = models.Foreignkey(Wager)
-    # user = models.ForeignKey(User)
+    entry = models.ForeignKey(Entry)
+    wager = models.ForeignKey(Wager)
+    user = models.ForeignKey(User)
