ef12e3c31d6b2914baa8904ea5380ca104be062d
Kevin Morgan <kevin.morgan@hogarthww.com>
2011-02-21 21:33:49 +0000

    Adding pick id to pick view.

--- urls.py
+++ urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     url(r'^$', 'wager.views.index', name='home'),
     url(r'^(?P<slug>[a-z0-9\-_]+)/$', 'wager.views.wager', name='wager'),
-    url(r'^(?P<wager_slug>[a-z0-9\-_]+)/pick/(?P<user_slug>[a-z0-9\-_]+)/(?P<user_hash>[a-zA-Z0-9\-_]+)/$', 'wager.views.pick', name='pick'),
+    url(r'^(?P<wager_slug>[a-z0-9\-_]+)/pick/(?P<user_slug>[a-z0-9\-_]+)/(?P<user_hash>[a-zA-Z0-9\-_]+)/(?P<pick_id>[0-9]+)?/?$', 'wager.views.pick', name='pick'),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT})
 )
